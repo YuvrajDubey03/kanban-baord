8cb0388fd3105d0d9c45b5a149281c47b9e4c22d
YuvrajDubey03 <yuvrajdubey21@gmail.com>
2026-01-03 17:51:48 +0530

    add more working logic and functionality

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const todo =document.querySelector("#Todo");
 const progress =document.querySelector("#Progress");
 const done =document.querySelector("#Done");
 let dragElement = null;
-
+const columns = [todo, progress, done];
 
 
 const tasks = document.querySelectorAll(".task");
@@ -30,8 +30,62 @@ function addDragEventsonColumn(column){
         e.preventDefault();
         column.appendChild(dragElement);
         column.classList.remove("hover-over");
+
+         columns.forEach(col => {
+            const tasks = col.querySelectorAll(".task");
+            const count = col.querySelector(".right");
+
+            count.textContent = tasks.length;
+         });
      });
 }
 addDragEventsonColumn(todo);
 addDragEventsonColumn(progress);
 addDragEventsonColumn(done);
+
+
+// modal
+const toggleModalButton = document.querySelector("#toggle-modal");
+const modalBG = document.querySelector(".bg");
+const modal = document.querySelector(".modal");
+const addTaskButton = document.querySelector("#add-new-task");
+
+
+toggleModalButton.addEventListener("click", (e) =>{
+    modal.classList.toggle("active");
+} );
+
+modalBG.addEventListener("click", (e) =>{
+    modal.classList.remove("active");
+} );
+
+
+
+
+addTaskButton.addEventListener("click", (e) =>{
+
+const taskTitle = document.querySelector("#task-title-input").value;
+const taskDesc = document.querySelector("#task-desc-input").value;
+
+const div = document.createElement("div");
+div.classList.add("task");
+div.setAttribute("draggable", "true");
+
+div.innerHTML = `<h2>${taskTitle}</h2>
+                <p>${taskDesc}</p>
+                <button>Delete</button>`;
+todo.appendChild(div);
+columns.forEach(col => {
+            const tasks = col.querySelectorAll(".task");
+            const count = col.querySelector(".right");
+
+            count.textContent = tasks.length;
+         });
+modal.classList.remove("active");
+
+div.addEventListener("drag", (e) =>{
+    dragElement = div;
+
+})
+
+});
